6493ff4650b443023e7a2637cd728d98b50ab3fe
Barlog <barlog@tanelorn.li>
2018-04-02 18:49:47 +0300

    Fix test

--- src/test/kotlin/app/customer/CustomerServiceTest.kt
+++ src/test/kotlin/app/customer/CustomerServiceTest.kt
@@ -10,25 +10,27 @@ import reactor.core.publisher.Mono
 
 class CustomerServiceTest {
     private val repository = mock(CustomerRepository::class.java)
+    private val service = CustomerService(repository)
     private val customer = generateCustomer()
 
     @Test
     fun findBy() {
         given(repository.findBy(customer.name)).willReturn(Flux.just(customer))
-        repository.findBy(customer.name)
+        service.findBy(customer.name)
         verify(repository, times(1)).findBy(customer.name)
     }
 
     @Test
     fun save() {
         given(repository.save(customer)).willReturn(Mono.just(customer))
-        repository.save(customer)
+        service.save(customer)
         verify(repository, times(1)).save(customer)
     }
 
     @Test
     fun deleteById() {
-        repository.deleteById(customer.id)
+        given(repository.deleteById(customer.id)).willReturn(Mono.empty())
+        service.deleteById(customer.id)
         verify(repository, times(1)).deleteById(customer.id)
     }
 }
