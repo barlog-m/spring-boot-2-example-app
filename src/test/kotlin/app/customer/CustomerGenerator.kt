fcef4943cf8a0abe30a26943ab30c33fabc15939
Barlog <barlog@tanelorn.li>
2018-04-03 15:07:52 +0300

    Fix customer generator

--- src/test/kotlin/app/customer/CustomerGenerator.kt
+++ src/test/kotlin/app/customer/CustomerGenerator.kt
@@ -1,11 +1,14 @@
 package app.customer
 
+import io.codearte.jfairy.Fairy
 import java.math.BigDecimal
 import java.time.LocalDate
+import java.util.concurrent.ThreadLocalRandom
 
 fun generateCustomer() =
     Customer(
-        name = "John Doe",
-        balance = BigDecimal("12.0"),
-        lastDeposit = LocalDate.of(2017, 11, 26)
+        name = Fairy.create().person().fullName,
+        balance = BigDecimal(ThreadLocalRandom.current().nextInt(1, 12000)),
+        lastDeposit = LocalDate.now()
+            .plusDays(ThreadLocalRandom.current().nextLong(1, 99))
     )
