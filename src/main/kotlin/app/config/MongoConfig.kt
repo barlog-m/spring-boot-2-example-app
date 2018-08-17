fb747c1d9ecd06db0ab5d671e19908f7a4e2bccf
Barlog <barlog@tanelorn.li>
2018-08-17 13:03:53 +0300

    1. Migrage to snake case. 2. Fix mongodb settings

--- src/main/kotlin/app/config/MongoConfig.kt
+++
@@ -1,8 +0,0 @@
-package app.config
-
-import org.springframework.context.annotation.Configuration
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
-
-@EnableReactiveMongoRepositories(basePackages = ["app"])
-@Configuration
-open class MongoConfig
